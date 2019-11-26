8861adba7e2f4054b6877fd4aeae0d9daac71c4f
ibrahim arslan <ibrahimarslann59@gmail.com>
2019-11-26 14:22:52 +0300

    Add files via upload
    a

---
+++ rec.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *dizi[] = {"Ankara","Mardin","Edirne"};
+
+int sayac = 0;
+int recursive (char * dizi ) {
+	
+}
+int main(){
+	
+	for(int i=0; i<3; i++) {
+		printf("%s", dizi[i] );
+
+	}
+
+}	
+
